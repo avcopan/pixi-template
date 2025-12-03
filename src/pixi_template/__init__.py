b51eda62c60b97e15ffb4a00a5e2375f15e5c03c
avcopan <avcopan@gmail.com>
2025-12-03 14:59:29 -0500

    Add and configure bumpver

--- src/pixi_template/__init__.py
+++ src/pixi_template/__init__.py
@@ -1,1 +1,3 @@
 """pixi_template."""
+
+__version__ = "0.2024.0"
--- tests/test_stub.py
+++ tests/test_stub.py
@@ -1,5 +1,8 @@
 """Tests for the pixi_example package."""
 
+import pixi_template
+
 
 def test_stub() -> None:
     """Stub test to ensure the test suite runs."""
+    print(pixi_template.__version__)  # noqa: T201
